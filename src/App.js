e4f4f13f0223694440f4d427c4c5d0c5d1718758
Viet Do <viet>
2020-01-07 09:39:07 +0700

    Create react app with docker

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
-import Header from './components/Header/Header'
-import Content from './components/Content/Content'
 function App() {
   return (
     <div className="App">
-      <div className="Header">
-        <Header></Header>
-      </div>
-      <div className="panel">
-        <Content></Content>
-      </div>
+      <p>Hello world!</p>
     </div>
   );
 }
